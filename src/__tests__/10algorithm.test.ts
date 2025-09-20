f43eacbee085e7da63b38def30472395f7851ba2
Hauke D <haukex@zero-g.net>
2025-09-20 19:46:31 +0200

    Began work on algorithm

---
+++ src/__tests__/10algorithm.test.ts
@@ -0,0 +1,152 @@
+/** Tests for Ranking Algorithms
+ *
+ * Copyright © 2025 Hauke D (haukex@zero-g.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+import { test, expect } from '@playwright/test'
+import { normalizeScores, findTieGroups, Comparator, compareAllSort, breakTies } from '../algorithm'
+
+test('normalizeScores', async () => {
+  // function doesn't care about the strings, and sorts the array in place, so make a quick helper:
+  const norm = (scores :number[]) => {
+    const arr :[string, number][] = scores.map(e => ['', e])
+    normalizeScores(arr)
+    return arr.map(e => e[1])
+  }
+  expect( norm([]) ).toStrictEqual([])
+  expect( norm([0]) ).toStrictEqual([0])
+  expect( norm([1]) ).toStrictEqual([0])
+  expect( norm([42]) ).toStrictEqual([0])
+  expect( norm([0,1]) ).toStrictEqual([0,1])
+  expect( norm([0,0]) ).toStrictEqual([0,0])
+  expect( norm([5,10]) ).toStrictEqual([0,1])
+  expect( norm([5,5]) ).toStrictEqual([0,0])
+  expect( norm([0,1,2,3]) ).toStrictEqual([0,1,2,3])
+  expect( norm([5,10,15,20]) ).toStrictEqual([0,1,2,3])
+  expect( norm([10,10,10,10]) ).toStrictEqual([0,0,0,0])
+  expect( norm([5,10,10,15]) ).toStrictEqual([0,1,1,2])
+  expect( norm([5,5,10,10]) ).toStrictEqual([0,0,1,1])
+  expect( norm([5,5,10,15,15]) ).toStrictEqual([0,0,1,2,2])
+  expect( norm([5,10,10,15,20,20,20,25]) ).toStrictEqual([0,1,1,2,3,3,3,4])
+  expect( norm([5,5,10,15,15,15,20,25,25]) ).toStrictEqual([0,0,1,2,2,2,3,4,4])
+  expect( norm([0,0,1,2,2,2,3,4,4]) ).toStrictEqual([0,0,1,2,2,2,3,4,4])
+})
+
+test('scoreGroups', async () => {
+  expect( findTieGroups([]) ).toStrictEqual([])
+  expect( findTieGroups([['A',1]]) ).toStrictEqual([])
+  expect( findTieGroups([['A',1],['B',1]]) ).toStrictEqual([[0,2]])
+  expect( findTieGroups([['A',1],['B',1],['C',1]]) ).toStrictEqual([[0,3]])
+  expect( findTieGroups([['A',1],['B',2],['C',3],['D',4]]) ).toStrictEqual([])
+  expect( findTieGroups([['A',1],['B',2],['C',2],['D',3]]) ).toStrictEqual([[1,3]])
+  expect( findTieGroups([['A',1],['B',1],['C',2],['D',2]]) ).toStrictEqual([[0,2],[2,4]])
+  expect( findTieGroups([['A',1],['B',1],['C',2],['D',3],['E',3]]) ).toStrictEqual([[0,2],[3,5]])
+  expect( findTieGroups([['A',0],['B',0],['C',1],['D',2],['E',2]]) ).toStrictEqual([[0,2],[3,5]])
+  expect( findTieGroups([
+    ['A',1],['B',2],['C',2],['D',3],['E',4],['F',4],['G',4],['H',5]
+  ]) ).toStrictEqual([[1,3],[4,7]])
+  expect( findTieGroups([
+    ['A',1],['B',2],['C',2],['D',3],['E',4],['F',4],['G',4],['H',4]
+  ]) ).toStrictEqual([[1,3],[4,8]])
+  expect( findTieGroups([
+    ['A',1],['B',2],['C',2],['D',3],['E',4],['F',4],['G',4],['H',5],['I',5],['J',5]
+  ]) ).toStrictEqual([[1,3],[4,7],[7,10]])
+})
+
+function makeSimpleComp(items :string[]) :Comparator {
+  return ([a,b]) => Promise.resolve( items.indexOf(a) > items.indexOf(b) ? 0 : 1 )
+}
+
+test('makeSimpleComp', async () => {
+  const comp = makeSimpleComp(['A','B','C'])
+  expect( await comp(['A','B']) ).toStrictEqual(1)
+  expect( await comp(['A','C']) ).toStrictEqual(1)
+  expect( await comp(['B','A']) ).toStrictEqual(0)
+  expect( await comp(['B','C']) ).toStrictEqual(1)
+  expect( await comp(['C','A']) ).toStrictEqual(0)
+  expect( await comp(['C','B']) ).toStrictEqual(0)
+})
+
+function makeCustomComp(items :Record<string, 0|1>) :Comparator {
+  const m = new Map(Object.entries(items))
+  return ab => {
+    const swap = ab[0] > ab[1]
+    const k = swap ? ab[1]+'\0'+ab[0] : ab[0]+'\0'+ab[1]
+    const r = m.get(k)
+    if (r!=undefined)
+      return Promise.resolve(swap ? (r ? 0 : 1) : r)
+    throw new Error(`Unhandled comparison a=${ab[0]} b=${ab[1]}`)
+  }
+}
+
+test('makeCustomComp', async () => {
+  const comp = makeCustomComp({'A\0B':0,'A\0C':1,'B\0C':0})
+  // Alice beats Bob
+  expect( await comp(['A','B']) ).toStrictEqual(0)
+  expect( await comp(['B','A']) ).toStrictEqual(1)
+  // Carol beats Alice
+  expect( await comp(['A','C']) ).toStrictEqual(1)
+  expect( await comp(['C','A']) ).toStrictEqual(0)
+  // Bob beats Carol
+  expect( await comp(['B','C']) ).toStrictEqual(0)
+  expect( await comp(['C','B']) ).toStrictEqual(1)
+})
+
+test('compareAllSort', async () => {
+  // simple sort
+  const comp = makeSimpleComp(['A','B','C','D'])
+  expect( await compareAllSort([], comp) ).toStrictEqual([])
+  expect( await compareAllSort(['B','A'], comp) ).toStrictEqual([ ['A',0], ['B',1] ])
+  expect( await compareAllSort(['A','B'], comp) ).toStrictEqual([ ['A',0], ['B',1] ])
+  expect( await compareAllSort(['C','D','B','A'], comp) )
+    .toStrictEqual([ ['A',0], ['B',1], ['C',2], ['D',3] ])
+  expect( await compareAllSort(['A','B','C','D'], comp) )
+    .toStrictEqual([ ['A',0], ['B',1], ['C',2], ['D',3] ])
+  expect( await compareAllSort(['D','C','B','A'], comp) )
+    .toStrictEqual([ ['A',0], ['B',1], ['C',2], ['D',3] ])
+
+  // three-way tie
+  const compTie = makeCustomComp({'A\0B':0,'A\0C':1,'B\0C':0})
+  expect( await compareAllSort(['C','A','B'], compTie) )
+    .toStrictEqual([ ['A',0], ['B',0], ['C',0] ])
+  expect( await compareAllSort(['B','C','A'], compTie) )
+    .toStrictEqual([ ['A',0], ['B',0], ['C',0] ])
+  expect( await compareAllSort(['A','B','C'], compTie) )
+    .toStrictEqual([ ['A',0], ['B',0], ['C',0] ])
+  expect( await compareAllSort(['C','B','A'], compTie) )
+    .toStrictEqual([ ['A',0], ['B',0], ['C',0] ])
+})
+
+const compFail :Comparator = _ab => { throw new Error('I shouldn\'t be called in this test') }
+
+test('breakTies', async () => {
+  expect( await breakTies([], compFail) ).toStrictEqual([])
+  expect( await breakTies([['A',1]], compFail) ).toStrictEqual([['A',1]])
+  expect( await breakTies([['A',1],['B',2]], compFail) ).toStrictEqual([['A',1],['B',2]])
+  expect( await breakTies([['A',1],['B',2],['C',3]], compFail) ).toStrictEqual([['A',1],['B',2],['C',3]])
+
+  const comp = makeSimpleComp(['G','F','E','D','C','B','A'])
+  expect( await breakTies([['A',5],['B',5]], comp) ).toStrictEqual([['B',0],['A',1]])
+  expect( await breakTies([['A',5],['B',5],['C',5]], comp) ).toStrictEqual([['C',0],['B',1],['A',2]])
+  expect( await breakTies([['X',0],['A',1],['B',1],['C',1],['Y',2]], comp) ).toStrictEqual([['X',0],['C',1],['B',2],['A',3],['Y',4]])
+  expect( await breakTies([['X',3],['A',5],['B',5],['C',5],['Y',10]], comp) ).toStrictEqual([['X',3],['C',4],['B',5],['A',6],['Y',7]])
+  expect( await breakTies([['A',0],['B',0],['X',1],['Y',2],['C',3],['D',3]], comp) )
+    .toStrictEqual([ ['B',0],['A',1],['X',2],['Y',3],['D',4],['C',5] ])
+  /* The following scores may seem strange at first, but the reason is the delta between X and Y's scores in the input.
+   * These kinds of scores are not expected anyway, since the functions in this module always normalize them.
+   * These scores are basically just helper values for sorting and identifying ties! */
+  expect( await breakTies([['A',5],['B',5],['X',10],['Y',15],['C',20],['D',20]], comp) )
+    .toStrictEqual([ ['B',0],['A',1],['X',2],['Y',7],['D',8],['C',9] ])
+})
---
+++ src/algorithm.ts
@@ -0,0 +1,129 @@
+/** Ranking Algorithms
+ *
+ * Copyright © 2025 Hauke D (haukex@zero-g.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+import { assert, paranoia } from './utils'
+
+/** An array of tuples of (item, score). Scores must be >=0 and there may not be duplicate items. */
+export type RankedResults = [item: string, score: number][]
+
+/** Sort the given results **in-place**, first by score, second by string. */
+export function sortResults(results :RankedResults) {
+  results.sort(([a1, a2], [b1, b2]) => a2 - b2 || a1.localeCompare(b1))
+}
+
+/** Normalize the scores **in-place** so they start at zero and there are no gaps; ties are kept.
+ *
+ * @param results The ranked results, **must** already be sorted by score!
+ *  (Note this function does not care about or touch the strings that are the first item of the result tuples.)
+ */
+export function normalizeScores(results :RankedResults) {
+  let prevScore :number = -1
+  let curScore :number = -1
+  for(const item of results) {
+    if ( item[1]!=prevScore ) {
+      prevScore = item[1]
+      item[1] = ++curScore
+    }
+    else item[1] = curScore
+  }
+}
+
+/** Find groups of more than one identical score.
+ *
+ * @param results The ranked results, **must** already be sorted by score!
+ * @returns The index ranges of runs of more than one identical score (start inclusive, end exclusive).
+ */
+export function findTieGroups(results :Readonly<RankedResults>) :[first_incl: number, last_excl: number][] {
+  const groups :[number,number][] = []
+  let prevScore :number = -1
+  let prevIdx :number = -1
+  results.forEach((item, i) => {
+    if ( item[1]!=prevScore ) {  // score changed
+      if ( i>prevIdx+1 )  // run of longer than one
+        groups.push([prevIdx,i])
+      prevScore = item[1]
+      prevIdx = i
+    }
+  })
+  if (prevIdx<results.length-1)
+    groups.push([prevIdx,results.length])
+  return groups
+}
+
+/** A function to compare two items. Should not be called with the same item for both `a` and `b`.
+ * Must return a Promise resolving to 0 if item `a` is ranked higher, or 1 if item `b` is ranked higher. */
+export type Comparator = (ab :Readonly<[a :string, b :string]>) => Promise<0|1>
+
+export async function compareAllSort(items :Readonly<string[]>, comparator :Comparator) :Promise<RankedResults> {
+  if (new Set(items).size != items.length)
+    throw new Error('No duplicates allowed in items to be ranked')
+  const scores: Map<string, number> = new Map(items.map(e => [e, 0]))
+  //TODO Later: A merge-insertion sort version to minimize comparisons, https://en.wikipedia.org/wiki/Merge-insertion_sort
+  for (let i = 0; i < items.length; i++) {
+    for (let j = i+1; j < items.length; j++) {
+      const a = items[i]
+      const b = items[j]
+      assert(a!=undefined && b!=undefined)
+      const c = await comparator([a,b]) ? b : a
+      const s = scores.get(c)
+      scores.set( c, s ? s + 1 : 1 )
+    }
+  }
+  const results = Array.from(scores)
+  sortResults(results)
+  normalizeScores(results)
+  return results
+}
+
+export async function breakTies(results :Readonly<RankedResults>, comparator :Comparator) :Promise<RankedResults> {
+  if (new Set(results.map(([s,])=>s)).size != results.length)
+    throw new Error('No duplicates allowed in items to be ranked')
+  const res :RankedResults = Array.from(results)
+  await Promise.all( findTieGroups(res).map( async ([first,after]) => {
+    // in this group, all the results are tied on their score, so force the user to re-evaluate
+    const subRes = await compareAllSort(res.slice(first, after).map(([s,])=>s), comparator)
+    // splice the sub-results back into the array, adjusting the scores accordingly
+    const baseScoreBefore :number = (() => {
+      if (first<=0) return 0
+      const r = res[first-1]
+      assert(r!=undefined)
+      return r[1]+1 })()
+    paranoia(after>first && after-first===subRes.length)
+    let gi :number = 0
+    for( let ri=first; ri<after; ri++ ) {
+      const r = res[ri]
+      const s = subRes[gi++]
+      assert(r!=undefined && s!=undefined)
+      r[0] = s[0]
+      r[1] = s[1] + baseScoreBefore
+    }
+    paranoia(gi===subRes.length)
+    // adjust the scores of all items following the spliced group (if any)
+    if (after<results.length) {
+      const sl = res[after-1]  // the last item spliced in
+      const rr = res[after]    // the first item to adjust
+      assert(sl!=undefined && rr!=undefined)
+      const baseScoreAfter = sl[1] + 1 - rr[1]
+      for( let ri=after; ri<results.length; ri++ ) {
+        const r = res[ri]
+        assert(r!=undefined)
+        r[1] += baseScoreAfter
+      }
+    }
+  }))
+  return res
+}
