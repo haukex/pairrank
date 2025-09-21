c19f03ef13cac025f9d5c99abe2c82c9abd5b6ec
Hauke D <haukex@zero-g.net>
2025-09-21 14:21:28 +0200

    Extended permutation tests

--- src/__tests__/15ford-johnson.test.ts
+++ src/__tests__/15ford-johnson.test.ts
@@ -61,7 +61,11 @@ test('permutations', async () => {
   expect( Array.from(permutations(['A','B'])) ).toStrictEqual([ ['A','B'],['B','A'] ])
   expect( Array.from(permutations(['A','B','C'])) ).toStrictEqual([
     ['A','B','C'],['B','A','C'],['C','A','B'],['A','C','B'],['B','C','A'],['C','B','A'] ])
-  //for (const perm of permutations(['A','B','C'])) console.log(perm)
+  expect( Array.from(permutations(['A','B','C','D'])) ).toStrictEqual([
+    ['A','B','C','D'],['B','A','C','D'],['C','A','B','D'],['A','C','B','D'],['B','C','A','D'],['C','B','A','D'],
+    ['D','B','A','C'],['B','D','A','C'],['A','D','B','C'],['D','A','B','C'],['B','A','D','C'],['A','B','D','C'],
+    ['A','C','D','B'],['C','A','D','B'],['D','A','C','B'],['A','D','C','B'],['C','D','A','B'],['D','C','A','B'],
+    ['D','C','B','A'],['C','D','B','A'],['B','D','C','A'],['D','B','C','A'],['C','B','D','A'],['B','C','D','A'] ])
 })
 
 test('mergeInsertionSort', async () => {
